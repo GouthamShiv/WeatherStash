45bc04dd8cb71e84138121ff22d1b1fdd29f0620
Goutham S <gouthamshine@gmail.com>
2022-06-20 13:49:43 +0530

    Initial Commit

---
+++ WeatherStash/WeatherStashApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherStashApp.swift
+//  WeatherStash
+//
+//  Created by Goutham on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherStashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
